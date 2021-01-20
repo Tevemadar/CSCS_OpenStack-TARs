6e9f52c8826035967485b0b0c3a674aafd30a986
Gergely Csucs <Tevemadar@users.noreply.github.com>
2021-01-20 12:45:29 +0100

    initial commit

---
+++ CSCS_TARs.py
@@ -0,0 +1,53 @@
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from keystoneauth1.extras._saml2 import V3Saml2Password
+from keystoneclient.v3 import client
+import swiftclient.client as swiftclient
+from getpass import getpass
+import requests,glob,time,sys,os
+
+if not os.path.exists("done"):
+  os.mkdir("done")
+
+container = sys.argv[1]
+username = input('User name: ')
+password = getpass()
+
+batch=time.monotonic()-1
+with open(str(int(time.time()))+".txt","w") as logfile:
+  logfile.write(container)
+  logfile.write("\r\n")
+  for tar in glob.glob("*.tar"):
+    print(time.asctime())
+    print(tar)
+    logfile.write(time.asctime())
+    logfile.write("\r\n")
+    if batch<time.monotonic():
+      logfile.write("login "+username)
+      logfile.write("\r\n")
+      batch=time.monotonic()+60*60
+      auth = V3Saml2Password(auth_url='https://pollux.cscs.ch:13000/v3',
+                             identity_provider='cscskc',
+                             protocol='mapped',
+                             identity_provider_url='https://auth.cscs.ch/auth/realms/cscs/protocol/saml/',
+                             username=username,
+                             password=password)
+      sess = session.Session(auth=auth)
+      token = sess.get_token()
+    logfile.write(tar)
+    logfile.write("\r\n")
+    with open(tar,"rb") as tarfile:
+      req=requests.put("https://object.cscs.ch/v1/AUTH_08c08f9f119744cbbf77e216988da3eb/"+container+"/"+tar+"?extract-archive=tar",
+                       data=tarfile,
+                       headers={"X-Auth-Token":token})
+    os.rename(tar,"done/"+tar)
+    logfile.write(time.asctime())
+    logfile.write("\r\n")
+    logfile.write(str(req.headers))
+    logfile.write("\r\n")
+    logfile.write(str(req.content))
+    logfile.write("\r\n")
+    req=requests.head("https://object.cscs.ch/v1/AUTH_08c08f9f119744cbbf77e216988da3eb/"+container,
+                      headers={"X-Auth-Token":token})
+    logfile.write(str(req.headers))
+    logfile.write("\r\n")
---
+++ README.md
@@ -0,0 +1,16 @@
+#Multi-TAR uploader for CSCS/Pollux
+
+This code is built on https://github.com/eth-cscs/openstack and has similar [dependencies](https://github.com/eth-cscs/openstack/tree/master/cli)
+
+* pre-configured for CSCS/Pollux authentication
+* pre-configured for the Image Service project (```https://object.cscs.ch/v1/AUTH_08c08f9f119744cbbf77e216988da3eb/```)
+* expects an already existing container provided as command-line parameter
+* user name and password are entered after start
+* code grabs all .tar files in the current directory and uploads them as [self-extracting](https://docs.openstack.org/mitaka/user-guide/cli_swift_archive_auto_extract.html)
+* a logfile is created with unique name (timestamp-based), recording the container name and progress (time, file, response headers, response content, and a head request following the upload)
+* no error handling is provided, but uploaded files are moved into a ```done``` directory (created if does not exist), so code can be simply re-run after Gateway Timeout or similar errors
+* code logs in again before starting a new upload if more than 1 hours passed since the previous login
+
+Example usage (assuming ```CSCS_TARs.py``` and the actual ```.tar``` files being in the current directory):
+
+    python CSCS_TARs.py mnm-test-bucket-1
